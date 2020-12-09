d595e0a3922b65332a17e431d9afc1d937d0bc24
dl-watson <dlarkinwatson@gmail.com>
2020-12-09 13:32:19 -0800

    adding comments for tested pgAdmin queries

--- sql/setup.sql
+++ sql/setup.sql
@@ -17,3 +17,15 @@ CREATE TABLE poems (
 
 -- INSERT INTO poet (name, date_of_birth, date_of_death) VALUES ('Sylvia Plath', 1932, 1963);
 -- SELECT * FROM poet;
+
+-- INSERT INTO poems (author, title, text) 
+-- VALUES ('Sylvia Plath', 'Metaphors', 'I’m a riddle in nine syllables,
+-- An elephant, a ponderous house,
+-- A melon strolling on two tendrils.
+-- O red fruit, ivory, fine timbers!
+-- This loaf’s big with its yeasty rising.
+-- Money’s new-minted in this fat purse.
+-- I’m a means, a stage, a cow in calf.
+-- I’ve eaten a bag of green apples,
+-- Boarded the train there’s no getting off.');
+-- SELECT * FROM poems;
