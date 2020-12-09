5fa217bc7a4d873ac2e2ae71db80d7dc902613db
dl-watson <dlarkinwatson@gmail.com>
2020-12-09 13:52:34 -0800

    setting up basic test structure

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,17 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
-const request = require('supertest');
-const app = require('../lib/app');
+const fs = require("fs");
+const pool = require("../lib/utils/pool");
+const request = require("supertest");
+const app = require("../lib/app");
 
-describe('07_models-with-joins routes', () => {
+describe("07_models-with-joins routes", () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
   });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it("adds a new poet", async () => {});
+  it("adds a new poem", async () => {});
 });
