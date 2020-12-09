c62c38c1aca31af0ae9ceb617686b1d188e37ff7
dl-watson <dlarkinwatson@gmail.com>
2020-12-09 14:09:01 -0800

    modifying tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,6 +12,12 @@ describe("07_models-with-joins routes", () => {
     return pool.end();
   });
 
-  it("adds a new poet", async () => {});
+  it("adds a new poet", async () => {
+    const res = await request(app)
+      .post("/api/v1/poets")
+      .send({ hello: "world" });
+
+    expect(res.body).toEqual({ hello: "world" });
+  });
   it("adds a new poem", async () => {});
 });
