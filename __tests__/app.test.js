65003feaf9137669b3358ba55bba02ceeb4339bf
dl-watson <dlarkinwatson@gmail.com>
2020-12-09 16:52:35 -0800

    add new poet test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,9 @@ const pool = require("../lib/utils/pool");
 const request = require("supertest");
 const app = require("../lib/app");
 
+const Poets = require("../lib/models/Poets");
+const Poems = require("../lib/models/Poems");
+
 describe("07_models-with-joins routes", () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
@@ -13,11 +16,19 @@ describe("07_models-with-joins routes", () => {
   });
 
   it("adds a new poet", async () => {
-    const res = await request(app)
-      .post("/api/v1/poets")
-      .send({ hello: "world" });
+    const res = await request(app).post("/api/v1/poets").send({
+      poet: "Sylvia Plath",
+      dateOfBirth: 1932,
+      dateOfDeath: 1963,
+    });
 
-    expect(res.body).toEqual({ hello: "world" });
+    expect(res.body).toEqual({
+      id: "1",
+      poet: "Sylvia Plath",
+      dateOfBirth: 1932,
+      dateOfDeath: 1963,
+    });
   });
+
   it("adds a new poem", async () => {});
 });
