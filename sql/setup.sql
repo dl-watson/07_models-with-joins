06bc316984ad3fd341d6d5abaa4f990a74689ba4
dl-watson <dlarkinwatson@gmail.com>
2020-12-09 16:47:42 -0800

    cleaning up comments

--- sql/setup.sql
+++ sql/setup.sql
@@ -10,23 +10,7 @@ CREATE TABLE poet (
 
 CREATE TABLE poems (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    -- FOREIGN KEY(id) REFERENCES poet(id),
     author TEXT REFERENCES poet(poet),
     title TEXT NOT NULL, 
     text TEXT NOT NULL
 );
-
--- INSERT INTO poet (name, date_of_birth, date_of_death) VALUES ('Sylvia Plath', 1932, 1963);
--- SELECT * FROM poet;
-
--- INSERT INTO poems (author, title, text) 
--- VALUES ('Sylvia Plath', 'Metaphors', 'I’m a riddle in nine syllables,
--- An elephant, a ponderous house,
--- A melon strolling on two tendrils.
--- O red fruit, ivory, fine timbers!
--- This loaf’s big with its yeasty rising.
--- Money’s new-minted in this fat purse.
--- I’m a means, a stage, a cow in calf.
--- I’ve eaten a bag of green apples,
--- Boarded the train there’s no getting off.');
--- SELECT * FROM poems;
